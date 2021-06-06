abe21fb5b285e766d2549ab8e862744ff5f4935c
vortiz4 <71564102+vortiz4@users.noreply.github.com>
2021-06-05 22:54:08 -0500

    Added filler copy

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,15 @@ import React from "react";
 const HomePage = () => (
 	<>
 		<h1>Hello, welcome to my blog!</h1>
-		<p>Lorem Ipsum</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget sagittis purus. Nulla sit
+			amet hendrerit risus. Donec ac maximus lectus. Vestibulum convallis augue eu turpis semper
+			ultricies. Proin congue elit sed ultrices ultricies. Sed et sodales nulla, non viverra lectus.
+			Etiam ornare, lorem sed commodo auctor, ipsum risus rutrum turpis, at dapibus nunc turpis nec
+			ipsum. Sed dignissim mollis sem, et facilisis purus vulputate ac. Aliquam consectetur congue
+			felis, tristique scelerisque lacus porta id. Cras ut sem ut nisi aliquam consequat posuere a
+			lacus.
+		</p>
 	</>
 );
 

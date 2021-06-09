1366d517f5eb23a2dadd15d0268ced76cc3af9f7
vortiz4 <71564102+vortiz4@users.noreply.github.com>
2021-06-08 22:28:09 -0500

    Added a related articles section and a new component folder!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import HomePage from "./pages/HomePage";
+import NavBar from "./NavBar";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
 class App extends Component {
@@ -11,11 +13,15 @@ class App extends Component {
 		return (
 			<Router>
 				<div className="App">
+					<NavBar />
 					<div id="page-body">
-						<Route path="/" component={HomePage} exact />
-						<Route path="/about" component={AboutPage} />
-						<Route path="/articles-list" component={ArticlesList} />
-						<Route path="/article" component={ArticlePage} />
+						<Switch>
+							<Route path="/" component={HomePage} exact />
+							<Route path="/about" component={AboutPage} />
+							<Route path="/articles-list" component={ArticlesListPage} />
+							<Route path="/article/:name" component={ArticlePage} />
+							<Route component={NotFoundPage} />
+						</Switch>
 					</div>
 				</div>
 			</Router>
